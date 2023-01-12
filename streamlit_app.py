68e96d4b81a188b8b9820678dd74b56370cd4ad4
PengCLiew <122500179+PengCLiew@users.noreply.github.com>
2023-01-12 14:05:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
+import panda
+
 streamlit.title('Hello World')
 
 streamlit.header('🥣 Hello World')
 
 streamlit.text('🥗 Hello World')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
